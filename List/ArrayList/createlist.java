8e4beaf4b162cf1400a56edc95aa2e8bf5dd4b15
jatinkahar539 <jatinkahar539@gmail.com>
2026-04-06 22:18:13 +0530

    new file added

---
+++ List/ArrayList/createlist.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class createlist{
+    public static void main(String[] args){
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(15);
+        list.add(19);
+        list.add(10);
+        System.out.println(list);
+    }
+}
